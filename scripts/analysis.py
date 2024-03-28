1cebc0f2a6a76566644430211d4e4e8e2af955fe
Dan McPherson <dmcphers@redhat.com>
2024-03-28 10:36:00 -0400

    Convert analysis to a python script
    Signed-off-by: Dan McPherson <dmcphers@redhat.com>

---
+++ scripts/analysis.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+import os
+import click
+from matplotlib import pyplot as plt
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('max_colwidth', None)
+
+plt.style.use("bmh")
+
+
+def gather_pr_bench(workspace_dir, data_dir, data_dir_old, output_dir):
+    old_model_dir_pr_bench = os.path.join(data_dir_old, "pr_bench")
+    model_dir_pr_bench = os.path.join(data_dir, "pr_bench")
+
+    fn_q = os.path.join(model_dir_pr_bench, "question.jsonl")
+    df_q = pd.read_json(fn_q, lines=True)
+    print(f"{len(df_q)=}")
+
+    fn_q_old = os.path.join(old_model_dir_pr_bench, "question.jsonl")
+    df_q_old = pd.read_json(fn_q_old, lines=True)
+    print(f"{len(df_q_old)=}")
+
+    fn_j = os.path.join(model_dir_pr_bench, "gpt-4_single.jsonl")
+    df_j = pd.read_json(fn_j, lines=True)
+    print(f"{len(df_j)=}")
+
+    df_q_diff = df_q[~df_q['qna_fn'].isin(df_q_old["qna_fn"].to_list())]
+
+    df_q_diff.head().to_csv(os.path.join(output_dir, "question_diff.csv"),
+                            sep=',', index=False, encoding='utf-8')
+
+    print(len(df_q_diff["qna_fn"].unique()), len(df_q_diff["qna_fn"]))
+
+    df_q_diff.groupby("qna_fn").size().value_counts(
+    ).reset_index().rename({"index": "#examples"}, axis=1)
+
+    df_j_diff = df_j.merge(df_q_diff, on="question_id", how="inner")
+    df_j_diff['model'] = df_j_diff['model'].str.replace(
+        '-[0-9]$', '', regex=True)
+    df_j_diff['qna_fn'] = df_j_diff['qna_fn'].str.replace(
+        workspace_dir + '/taxonomy/', '')
+    df_j_diff['qna_fn'] = df_j_diff['qna_fn'].str.replace('/qna.yaml', '')
+
+    df_j_diff.groupby("model")["score"].mean().reset_index().to_csv(os.path.join(
+        output_dir, "pr_bench_scores.csv"), sep=',', index=False, encoding='utf-8')
+
+    df_g = df_j_diff.groupby(['qna_fn', 'model'])[
+        "score"].mean().unstack(fill_value=0)
+
+    df_g.head().to_csv(os.path.join(output_dir, "qna_scores.csv"),
+                       sep=',', index=True, encoding='utf-8')
+
+    if df_g.get("merlinite-7b") is not None:
+        fig = make_fig(df_g)
+        fig.savefig(os.path.join(output_dir, "qna_scores.png"))
+
+        fig = make_fig(df_g, 5.5)
+        fig.savefig(os.path.join(output_dir, "qna_scores_5.5.png"))
+
+
+def gather_mt_bench(data_dir, output_dir):
+    model_dir_mt_bench = os.path.join(output_dir, "mt_bench")
+
+    fn_q = os.path.join(model_dir_mt_bench, "question.jsonl")
+    df_q = pd.read_json(fn_q, lines=True)
+    print(f"{len(df_q)=}")
+
+    fn_j = os.path.join(model_dir_mt_bench, "gpt-4_single.jsonl")
+    df_j = pd.read_json(fn_j, lines=True)
+    print(f"{len(df_j)=}")
+
+    df_j['model'] = df_j['model'].str.replace('-[0-9]$', '', regex=True)
+    df_j.groupby("model")["score"].mean().to_csv(os.path.join(
+        output_dir, "mt_bench_scores.csv"), sep=',', index=True, encoding='utf-8')
+
+
+def make_fig(df_g, ths=10):
+    df_gg = df_g[df_g["merlinite-7b"] < ths]
+    n = df_gg.shape[0]
+
+    fig, ax = plt.subplots(figsize=(12, 0.4 * n + 1), tight_layout=True)
+    plt.close(fig)
+
+    y = list(range(n))
+    y0 = [yi + 0.15 for yi in y]
+    y1 = [yi - 0.15 for yi in y]
+    ax.vlines(ths, y[0] - 0.5, y[-1] + 0.5, color="gray",
+              linestyle="dashed", alpha=0.4)
+    for yi, m in zip([y0, y1], ["merlinite-7b", "merlinite-7b-rc"]):
+        ax.barh(yi, df_gg[m].to_list(), label=m, height=0.25)
+    ax.set_xlim(1, 10)
+    ax.set_yticks(y, df_gg.index.to_list())
+    ax.legend()
+
+    return fig
+
+
+@click.command()
+@click.option(
+    "--workspace-dir",
+    type=click.Path(),
+    default="/root/labrador-evaluation/ws",
+    show_default=True,
+    help="The workspace directory"
+)
+def main(workspace_dir):
+    data_dir = os.path.join(workspace_dir, "FastChat/fastchat/llm_judge/data")
+    data_dir_old = os.path.join(
+        workspace_dir, "FastChat/fastchat/llm_judge/data-old")
+    output_dir = data_dir
+
+    gather_pr_bench(workspace_dir, data_dir, data_dir_old, output_dir)
+    gather_mt_bench(data_dir, output_dir)
+
+
+if __name__ == "__main__":
+    main()
